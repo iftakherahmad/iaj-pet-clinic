6e412adb5e662c5f13868dc2f2e3da312aca6fc9
Md. Iftakher Ahmad <iftakhar.ahmed@therapservices.net>
2021-11-12 17:54:13 +0600

    Build multi module mvn project. Closes #1

--- pet-clinic-web/src/main/java/net/therap/iajpetclinic/IajPetClinicApplication.java
+++ pet-clinic-web/src/main/java/net/therap/iajpetclinic/IajPetClinicApplication.java
@@ -1,9 +1,11 @@
 package net.therap.iajpetclinic;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ManagementContextConfiguration
 public class IajPetClinicApplication {
 
     public static void main(String[] args) {
