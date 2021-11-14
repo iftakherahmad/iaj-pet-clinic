f9361f85ba8aecc79a4e29979bf5883a796fd240
Md. Iftakher Ahmad <iftakhar.ahmed@therapservices.net>
2021-11-15 01:22:30 +0600

    adding owners list and update controller. Closes #16

--- pet-clinic-web/src/main/java/net/therap/iajpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/net/therap/iajpetclinic/controller/OwnerController.java
@@ -1,19 +1,31 @@
 package net.therap.iajpetclinic.controller;
 
+import net.therap.iajpetclinic.model.Owner;
+import net.therap.iajpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 /**
  * @author iftakhar.ahmed
  * @since 11/13/21
  */
 @Controller
-@RequestMapping("/owners")
+@RequestMapping("/owners/")
 public class OwnerController {
 
-    @RequestMapping({"/","/index","/index.html"})
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"/","","/index","/index.html"})
     public String listOwners(Model model){
+        Set<Owner> owners=ownerService.findAll();
+        model.addAttribute("owners",owners);
         return "owners/index";
     }
 }
