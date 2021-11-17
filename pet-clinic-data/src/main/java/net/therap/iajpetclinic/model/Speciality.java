cf0c58c7414e9f6a7d51ac7f15b603ad5d70151e
Md. Iftakher Ahmad <iftakhar.ahmed@therapservices.net>
2021-11-17 13:08:33 +0600

    Added Speciality ,added to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/net/therap/iajpetclinic/model/Speciality.java
@@ -0,0 +1,18 @@
+package net.therap.iajpetclinic.model;
+
+/**
+ * @author iftakhar.ahmed
+ * @since 11/17/21
+ */
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/net/therap/iajpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/net/therap/iajpetclinic/model/Vet.java
@@ -1,8 +1,20 @@
 package net.therap.iajpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author iftakhar.ahmed
  * @since 11/11/21
  */
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
