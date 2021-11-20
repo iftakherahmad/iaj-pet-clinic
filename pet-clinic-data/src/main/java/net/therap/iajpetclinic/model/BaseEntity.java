f2c00e3ac8a0654091c58754223c57e86e4094a4
Md. Iftakher Ahmad <iftakhar.ahmed@therapservices.net>
2021-11-20 19:47:07 +0600

    Implemented base entity. Closes #35

--- pet-clinic-data/src/main/java/net/therap/iajpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/net/therap/iajpetclinic/model/BaseEntity.java
@@ -1,13 +1,17 @@
 package net.therap.iajpetclinic.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
  * @author iftakhar.ahmed
  * @since 11/12/21
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
