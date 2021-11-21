92b308443536f02ac87729bd9f8ac3b96c017d8d
Md. Iftakher Ahmad <iftakhar.ahmed@therapservices.net>
2021-11-21 11:08:19 +0600

    Adding visit service map.Closes #46

---
+++ pet-clinic-data/src/main/java/net/therap/iajpetclinic/service/VisitService.java
@@ -0,0 +1,11 @@
+package net.therap.iajpetclinic.service;
+
+import net.therap.iajpetclinic.model.Visit;
+import net.therap.iajpetclinic.service.map.AbstractMapService;
+
+/**
+ * @author iftakhar.ahmed
+ * @since 11/21/21
+ */
+public interface VisitService extends CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/net/therap/iajpetclinic/service/map/VisitServiceMap.java
@@ -0,0 +1,42 @@
+package net.therap.iajpetclinic.service.map;
+
+import net.therap.iajpetclinic.model.Visit;
+import net.therap.iajpetclinic.service.VisitService;
+
+import java.util.Set;
+
+/**
+ * @author iftakhar.ahmed
+ * @since 11/21/21
+ */
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet()==null||visit.getPet().getId()==null
+                ||visit.getPet().getOwner()==null
+                || visit.getPet().getOwner().getId()==null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
